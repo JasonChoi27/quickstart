e6ec9ad3c3f0693ed33e6e7177f86caab94587ab
JasonChoi27 <zhcgx4954@icloud.com>
2017-08-04 00:33:24 +0800

    add list to html templates

--- src/main/java/com/jason/choi/controller/HelloController.java
+++ src/main/java/com/jason/choi/controller/HelloController.java
@@ -1,9 +1,13 @@
 package com.jason.choi.controller;
 
+import com.jason.choi.model.JobInfo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -18,4 +22,37 @@ public class HelloController {
         return "hello";
     }
 
+    @RequestMapping("/table")
+    public String getTable(Model model){
+
+        JobInfo info = new JobInfo();
+        info.setJobId(1);
+        info.setJobName("test1");
+        info.setMainClass("com.jason.choi.main");
+        info.setAppId("applicationId-1001");
+        info.setJobStatus("running");
+
+        JobInfo info1 = new JobInfo();
+        info1.setJobId(2);
+        info1.setJobName("test2");
+        info1.setMainClass("com.jason.choi.Main");
+        info1.setAppId("applicationId-1002");
+        info1.setJobStatus("stopped");
+
+        JobInfo info2 = new JobInfo();
+        info2.setJobId(3);
+        info2.setJobName("test3");
+        info2.setMainClass("com.jason.choi.main");
+        info2.setAppId("applicationId-1003");
+        info2.setJobStatus("error");
+
+        List<JobInfo> jobs = new ArrayList<>();
+        jobs.add(info);
+        jobs.add(info1);
+        jobs.add(info2);
+
+        model.addAttribute("jobs", jobs);
+
+        return "hello";
+    }
 }
---
+++ src/main/java/com/jason/choi/model/JobInfo.java
@@ -0,0 +1,54 @@
+package com.jason.choi.model;
+
+/**
+ *
+ * Created by choi on 2017/8/3.
+ */
+public class JobInfo {
+
+    private int jobId;
+    private String jobName;
+    private String mainClass;
+    private String appId;
+    private String jobStatus;
+
+    public int getJobId() {
+        return jobId;
+    }
+
+    public void setJobId(int jobId) {
+        this.jobId = jobId;
+    }
+
+    public String getJobName() {
+        return jobName;
+    }
+
+    public void setJobName(String jobName) {
+        this.jobName = jobName;
+    }
+
+    public String getMainClass() {
+        return mainClass;
+    }
+
+    public void setMainClass(String mainClass) {
+        this.mainClass = mainClass;
+    }
+
+    public String getAppId() {
+        return appId;
+    }
+
+    public void setAppId(String appId) {
+        this.appId = appId;
+    }
+
+    public String getJobStatus() {
+        return jobStatus;
+    }
+
+    public void setJobStatus(String jobStatus) {
+        this.jobStatus = jobStatus;
+    }
+}
